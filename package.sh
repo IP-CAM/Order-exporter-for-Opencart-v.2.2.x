9514175c25fe594232bc829b9fb2267ff815108e
Richard Ma <richard.ma.19850509@gmail.com>
2018-04-03 14:24:59 +0800

    use install.tpl

--- package.sh
+++ package.sh
@@ -2,8 +2,13 @@
 
 rm rm-order-exporter.ocmod.zip
 
+# Replace modifycation ID
+sed "s/<code>.*<\/code>/<code><\!\[CDATA\[`git rev-parse HEAD`\]\]><\/code>/g" install.xml.tpl > install.xml
+
 zip rm-order-exporter.ocmod.zip \
     install.xml \
     LICENSE \
     README.md \
     -r upload
+
+rm install.xml
